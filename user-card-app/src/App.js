4a7378aba8623111b2b449e1c5e4a93345be316c
Angela Williams <lambda_school_loaner_97@Lambda-School-Loaner-97s-MacBook-Pro.local>
2019-10-07 01:42:35 -0700

    Added follow-along code to appjs

--- user-card-app/src/App.js
+++ user-card-app/src/App.js
@@ -1,40 +1,82 @@
+import "./App.css";
 import React, { Component } from 'react';
-import { Route, NavLink, BrowserRouter as Router } from 'react-router-dom';
-import './App.css';
 import axios from 'axios';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super()
+
+    // set some initial/default state values
     this.state = {
-      users: [],
-    };
+      breed: 'husky',
+      images: []
+    }
   }
 
-componentDidMount(){
-  axios.get().then(response => this.setState({smurfs : response.data})).catch(error => console.log(error));
-}
+  componentDidMount() {
+    // fetch images for the default state value (husky)
+    this.fetchDogImages()
+  }
 
-reloadUsers = (newUsers) => {
-  this.setState({
-    users: newUsers
-  })
-}
+  componentDidUpdate(prevProps, prevState) {
+    // this if statement prevents an infinite loop
+    // by only re-fetching if the breed has changed since the last update
+    //
+    // ----- same idea -----
+    //   useEffect(() => {
+    // 
+    //   }, [breed])
+    //
+    if (prevState.breed !== this.state.breed) {
+      // give the user feedback that something is happening
+      // by clearing out the current images while fetching new ones
+      this.setState({ images: [] })
 
-render() {
-  return (
-    <Router>
-      <div className = 'App'>
-      <div className = 'nav-bar'>
-      <NavLink to '/'>Users</NavLink>
-      <NavLink to '/user-form'>Add User</NavLink>
-      </div>
-      <Route exact path = '/' render = {(props) => <Users {...props} users = {this.state.users} />} />
-      <Route path = '/user-form' render = {(props) => <UserForm {...props} reloadUsers = {this.reloadUsers} />} />
-      </div>
-    </Router>
-  );
-}
+      // re-fetch images with new state value
+      this.fetchDogImages()
+    }
+  }
+
+  // extract this function from lifecycle event since it's used multiple times
+  fetchDogImages = () => {
+    axios.get(`https://dog.ceo/api/breed/${this.state.breed}/images`)
+      .then(result => {
+        this.setState({
+          images: result.data.message
+        })
+      })
+      .catch(error => {
+        console.log('error:', error)
+      })
+  }
+
+  // make our select field controlled by react state
+  handleChange = (event) => {
+    this.setState({
+      breed: event.target.value
+    })
+  }
+
+  render() {
+    return (
+      <>
+        <h1>The Dog Website</h1>
+
+        <select value={this.state.breed} onChange={this.handleChange}>
+          <option value="husky">Husky</option>
+          <option value="beagle">Beagle</option>
+          <option value="corgi">Corgi</option>
+          <option value="boxer">Boxer</option>
+        </select>
+
+        <div>
+          {this.state.images.map((image, index) => (
+            <img key={index} src={image} alt="Dog" />
+          ))}
+        </div>
+      </>
+    )
+  }
 }
 
 export default App;
